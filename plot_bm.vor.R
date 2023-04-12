d78970df284e4a20e739bedcf3eca4fe1a8b6986
Sean Di Stefano <sfperry14@gmail.com>
2023-04-12 09:56:34 -0600

    Update plot_bm.vor.R

--- plot_bm.vor.R
+++ plot_bm.vor.R
@@ -15,13 +15,21 @@ apex.bm_vor <- apexsad %>%
   merge(pastID_ecosite, by.x = "ID", by.y = "PastureID") %>%
   mutate(date = paste(Y, M, D, sep="-")) %>% # adding date column
   mutate(date = ymd(date)) %>% # transforming into date format
-  mutate(vor.bm = (STL + STD)*1000) # biomass left after grazing
+  mutate(vor.bm = (STL + STD)*1000) %>% 
+  filter(date >= "2014-01-01" & # filter for start of CARM
+           month(ymd(date)) %in% c(5:10)) %>% # filter for growing season
+  group_by(Y, date, Pasture) %>%
+  summarize(vor.bm = sum(vor.bm)) %>% # total standing biomass of a pasture
+  mutate(day = yday(date)) %>%
+  rename(Year = Y)
 
 ## RS biomass data, derived by Sean Kearney
 rs.biomass <- read.csv("cper_biomass_means_2014_2022.csv") %>%
   mutate(date = gsub(" 0:00", "", date)) %>% # removing unnecessary info
   mutate(date = mdy(date),
-         Biomass_kg_ha = Biomass_lbs_ac*1.121)
+         Biomass_kg_ha = Biomass_lbs_ac*1.121) %>% 
+  filter(date >= "2014-01-01" & # filter for start of CARM
+           month(ymd(date)) %in% c(5:10))
 
 ## Field VOR biomass data (biomass left after grazing)
 field.biomass_jun <- read.csv("CPER VOR/CARM_VOR_JUN_cln_attr_ALL_HiLo2022-10-17.csv") %>%
@@ -42,57 +50,28 @@ field.biomass_past <- field.biomass %>%
   summarize(bm_plot = mean(bm_transect)) %>% # biomass of each plot
   group_by(Year, Season, Pasture) %>% 
   summarize(bm_past = mean(bm_plot), # biomass of each pasture
-            bm_se = sd(bm_plot)/sqrt(length(bm_plot))) # SE across plots
-
-# adding date of collection
-field.biomass_past <- field.biomass_past %>%
+            bm_se = sd(bm_plot)/sqrt(length(bm_plot))) %>%
   mutate(month = ifelse(Season == "Spring", 6, 10)) %>% # add date based on season value
   mutate(date = paste(Year, month, 15, sep="-")) %>%
-  mutate(date = ymd(date))
+  mutate(date = ymd(date)) %>% # transforming to date format
+  filter(date >= "2014-01-01")
 
 ## NDVI
 ndvi_cper <- read.csv("cper_ndvi_means_2014_2022.csv") %>%
   mutate(date = gsub(" 0:00", "", date)) %>%
-  mutate(date = mdy(date))
-
-## Sub-setting data for pasture
-# function
-subset_past <- function(data, data.type, past_name) {
-  if(data.type == "APEX") {
-    past_bm <- data %>% filter(Pasture == past_name & 
-                                 date >= "2014-01-01" &
-                                 month(ymd(date)) %in% c(5:10)) %>%
-      group_by(Y, date, Pasture) %>%
-      summarize(vor.bm = sum(vor.bm)) %>%
-      mutate(day = yday(date)) %>%
-      rename(Year = Y)
-    
-    return(past_bm)
-  }
-  
-  if(data.type == "RS" | data.type == "NDVI" | data.type == "field") {
-    other_bm <- data %>% 
-      filter(Pasture == past_name) %>% 
-      filter(month(ymd(date)) %in% c(5:10) &
-               date >= "2014-01-01") %>%
-      mutate(day = yday(date))
-    
-    return(other_bm)
-  }
-}
-
-past15E_apex <- subset_past(data = apex.bm_vor, data.type = "APEX", past_name = "15E")
-past15E_rs <- subset_past(data = rs.biomass, data.type = "RS", past_name = "15E")
-past15E_ndvi <- subset_past(data = ndvi_cper, data.type = "NDVI", past_name = "15E")
-past15E_field <- subset_past(data = field.biomass_past, data.type = "field", past_name = "15E")
-
-past19N_apex <- subset_past(data = apex.bm_vor, data.type = "APEX", past_name = "19N")
-past19N_rs <- subset_past(data = rs.biomass, data.type = "RS", past_name = "19N")
-past19N_ndvi <- subset_past(data = ndvi_cper, data.type = "NDVI", past_name = "19N")
-past19N_field <- subset_past(data = field.biomass_past, data.type = "field", past_name = "19N")
+  mutate(date = mdy(date)) %>% # transforming to date format
+  filter(month(ymd(date)) %in% c(5:10) &
+           date >= "2014-01-01") %>% # filter for start of CARM
+  mutate(day = yday(date)) # adding day column
 
 ## plotting comparison results
-plot_bm.vor <- function(apex, rs, ndvi, field, past_name) {
+plot_bm.vor <- function(past_name) {
+  
+  apex <- apex.bm_vor %>% filter(Pasture == past_name)
+  rs <- rs.biomass %>% filter(Pasture == past_name)
+  ndvi <- ndvi_cper %>% filter(Pasture == past_name)
+  field <- field.biomass_past %>% filter(Pasture == past_name)
+  
   plot <- ggplot() +
     geom_line(data = apex, aes(x = date, y = vor.bm, color = "APEX")) +
     geom_line(data = rs, aes(x = date, y = Biomass_kg_ha, color = "Remote Sensing")) +
@@ -107,17 +86,14 @@ plot_bm.vor <- function(apex, rs, ndvi, field, past_name) {
     scale_x_date(date_breaks = "1 month",date_labels = "%m") +
     theme_bw() +
     ylab("VOR biomass (kg per ha)") +
-    xlab("Day of the Year") +
+    xlab("Month of the Year") +
     labs(color = "Data Source") +
     ggtitle(paste("Pasture", past_name))
   
   return(plot)
 }
 
-plot_bm.vor(apex = past15E_apex, rs = past15E_rs, 
-            ndvi = past15E_ndvi, field = past15E_field,
-            past_name = "15E")
+plot_bm.vor(past_name = "15E")
+plot_bm.vor(past_name = "19N")
 
-plot_bm.vor(apex = past19N_apex, rs = past19N_rs, 
-            ndvi = past19N_ndvi, field = past19N_field,
-            past_name = "19N")
+
